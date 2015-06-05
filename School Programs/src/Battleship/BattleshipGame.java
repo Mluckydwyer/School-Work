23460bdf66c7579021c1efe261876f1d2d5f42ba
Matt Dwyer <mluckydwyer@gmail.com>
2015-06-05 13:25:39 -0500

    Fix check place bugs

--- School Programs/src/Battleship/BattleshipGame.java
+++ School Programs/src/Battleship/BattleshipGame.java
@@ -4,144 +4,162 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class BattleshipGame {
-	
+
 	// Creates the arrays 10x10 for the boards
-	private static boolean[][]		playerShips	= new boolean[11][11];
-	private static boolean[][]		cpuShips	= new boolean[11][11];
-	private static boolean[][]		playerHits	= new boolean[11][11];
-	private static boolean[][]		cpuHits		= new boolean[11][11];
-	private static String[]			shipName	= new String[6];
-	private static int[]			shipLength	= new int[6];
-	
-	private static BattleshipGame	game;
-	private static Scanner			keyboardInt	= new Scanner(System.in);
-	private static Random			random;
-	
+	private static boolean[][] playerShips = new boolean[11][11];
+	private static boolean[][] cpuShips = new boolean[11][11];
+	private static boolean[][] playerHits = new boolean[11][11];
+	private static boolean[][] cpuHits = new boolean[11][11];
+	private static String[] shipName = new String[6];
+	private static int[] shipLength = new int[6];
+
+	private static BattleshipGame game;
+	private static Scanner keyboardInt = new Scanner(System.in);
+	private static Random random;
+
 	public static void main(String[] args) {
 		game = new BattleshipGame();
 		random = new Random();
 		int choice;
 		int cordX = 0;
 		int cordY = 0;
-		
+
 		boolean redo = false;
 		boolean orientation = false; // true for horizontal, false for vertical
-		
+
 		shipName[1] = "Patrol Boat";
 		shipName[2] = "Destroyer";
 		shipName[3] = "Submarine";
 		shipName[4] = "Battleship";
 		shipName[5] = "Aircraft Carrier";
-		
-		shipLength[1] = 2;
+
+		shipLength[1] = 2;// TESTTTTTTTTTTTTTTTTT
 		shipLength[2] = 3;
 		shipLength[3] = 3;
 		shipLength[4] = 4;
 		shipLength[5] = 5;
-		
+
 		System.out.println("Hello and welcome to Batleship");
 		game.clearBoard();
-		
+		game.cpuShipPlacement();
+
 		for (int x = 1; x < 6; x++) {
 			game.printBoard();
-			
+
 			do {
-				
+				redo = false;
 				System.out.println("How would you like to orient your " + shipName[x] + " that is " + shipLength[x] + " spaces long?");
 				System.out.println("[1] - Horizontaly (==)");
 				System.out.println("[2] - Verticaly (||)");
 				choice = keyboardInt.nextInt();
-				
+
 				if (choice < 1 && choice > 2) {
 					redo = true;
+				} else {
+					if (choice == 1)
+						orientation = true; // true for horizontal, false for
+											// vertical
+					else if (choice == 2)
+						orientation = false; // true for horizontal, false for
+												// vertical
 				}
-				else {
-					if (choice == 1) orientation = true; // true for horizontal, false for
-															// vertical
-					else if (choice == 2) orientation = false; // true for horizontal, false for
-																// vertical
-				}
-				
+
 				if (!redo) {
 					System.out.println("what cordinates would you like the uper left corrner of your " + shipName[x] + " that is " + shipLength[x] + " spaces long to be in (x then y)?");
 					cordX = keyboardInt.nextInt();
 					cordY = keyboardInt.nextInt();
 					// toggles the result, it does this because checkShip
 					// placement returns true if it is ok
-					redo = !game.checkShipPlacement(orientation, shipLength[x], cordX, cordY);
+					redo = !game.checkShipPlacement(orientation, shipLength[x], cordX, cordY, "player");
 				}
-				
+
 				if (redo) {
 					System.out.println("\f");
 					game.printBoard();
 					System.out.println("That isn't a valid loaction for that ship");
 				}
-				
+
 			} while (redo);
-			
+
 			for (int i = shipLength[x]; i > 0; i--) {
-				
-				if (i == shipLength[x]) playerShips[cordX][cordY] = true;
-				
-				if (orientation) playerShips[cordX + (shipLength[x] - i)][cordY] = true;
-				else if (!orientation) playerShips[cordX][cordY + (shipLength[x] - i)] = true;
+
+				if (i == shipLength[x])
+					playerShips[cordX][cordY] = true;
+
+				if (orientation)
+					playerShips[cordX + (shipLength[x] - i)][cordY] = true;
+				else if (!orientation)
+					playerShips[cordX][cordY + (shipLength[x] - i)] = true;
 			}
 		}
-		
+
 		game.cpuShipPlacement();
 	}
-	
+
 	private void cpuShipPlacement() {
 		boolean redo = false;
 		int cordX;
 		int cordY;
 		boolean orin;
-		
+
 		for (int x = 1; x < 6; x++) {
-			
+
 			do {
+				redo = false;
 				orin = random.nextBoolean();
 				cordX = random.nextInt(10) + 1;
 				cordY = random.nextInt(10) + 1;
-				
-				redo = !game.checkShipPlacement(orin, shipLength[x], cordX, cordY);
+
+				redo = !game.checkShipPlacement(orin, shipLength[x], cordX, cordY, "cpu");
 			} while (redo);
-			
+
 			for (int i = shipLength[x]; i > 0; i--) {
-				
-				if (i == shipLength[x]) playerShips[cordX][cordY] = true;
-				
-				if (orin) playerShips[cordX + (shipLength[x] - i)][cordY] = true;
-				else if (!orin) playerShips[cordX][cordY + (shipLength[x] - i)] = true;
+
+				if (i == shipLength[x])
+					cpuShips[cordX][cordY] = true;
+
+				if (orin)
+					cpuShips[cordX + (shipLength[x] - i)][cordY] = true;
+				else if (!orin)
+					cpuShips[cordX][cordY + (shipLength[x] - i)] = true;
 			}
 		}
 	}
-	
-	private boolean checkShipPlacement(boolean orin, int length, int cordX, int cordY) {
+
+	private boolean checkShipPlacement(boolean orin, int length, int cordX, int cordY, String player) {
 		// orin is true for horizontal, false for vertical
-		if (cordX < 1) return false;
-		if (cordY < 1) return false;
-		
+		if (cordX < 1)
+			return false;
+		if (cordY < 1)
+			return false;
+
 		if (orin) {
-			if (cordX > 11 - length) return false;
+			if (cordX + length > 11) return false;
 			if (cordY > 11) return false;
-		}
-		else if (!orin) {
+		} else if (!orin) {
 			if (cordX > 11) return false;
-			if (cordY > 11 - length) return false;
+			if (cordY + length > 11) return false;
 		}
-		
-		for (int i = length; i > 0; i--) {
-			if (orin && playerShips[cordX + i][cordY]) return false;
-			if (!orin && playerShips[cordX][cordY + i]) return false;
+
+		if (player.equals("player")) {
+			for (int i = length; i > 0; i--) {
+				if (orin && playerShips[cordX + i - 1][cordY]) return false;
+				if (!orin && playerShips[cordX][cordY + i - 1]) return false;
+			}
+		}
+		else if (player.equals("cpu")) {
+			for (int i = length; i > 0; i--) {
+				if (orin && cpuShips[cordX + i - 1][cordY]) return false;
+				if (!orin && cpuShips[cordX][cordY + i - 1]) return false;
+			}
 		}
 		return true;
 	}
-	
+
 	private void clearBoard() {
 		// Set all spaces to nothing
 		for (int x = 1; x < 11; x++) {
-			
+
 			for (int y = 1; y < 11; y++) {
 				playerShips[x][y] = false;
 				cpuShips[x][y] = false;
@@ -150,48 +168,54 @@ public class BattleshipGame {
 			}
 		}
 	}
-	
+
 	private void printBoard() {
 		String space = "ERROR";
-		
-		System.out.println("\tYour Ships\t\tYour Hits");
+
+		System.out.println("\tYour Ships\t\tCPU Ships");
 		System.out.println("   1 2 3 4 5 6 7 8 9 10\t   1 2 3 4 5 6 7 8 9 10");
-		
+
 		for (int y = 1; y < 11; y++) {
-			
-			if (y < 10 || y < 20) System.out.print(y + " ");
-			else System.out.print(y);
-			
+
+			if (y < 10 || y < 20)
+				System.out.print(y + " ");
+			else
+				System.out.print(y);
+
 			for (int x = 1; x < 11; x++) {
-				
+
 				if (playerShips[x][y]) {
 					space = "O";
-				}
-				else if (!playerShips[x][y]) {
+				} else if (!playerShips[x][y]) {
 					space = ".";
 				}
-				
-				if (y == 10 && x == 1) System.out.print("" + space);
-				else System.out.print(" " + space);
+
+				if (y == 10 && x == 1)
+					System.out.print("" + space);
+				else
+					System.out.print(" " + space);
 			}
-			
+
 			System.out.print("\t" + y);
-			// ///////////////////////Printing out CPU ships for testing purposes
+			// ///////////////////////Printing out CPU ships for testing
+			// purposes
 			for (int x = 1; x < 11; x++) {
-				
-				if (cpuHits[x][y]) {
+
+				if (cpuShips[x][y]) {
 					space = "O";
-				}
-				else if (!cpuHits[x][y]) {
+				} else if (!cpuShips[x][y]) {
 					space = ".";
 				}
-				
-				if (y == 10 && x == 1) System.out.print(" " + space);
-				else if (x == 1) System.out.print("  " + space);
-				else System.out.print(" " + space);
+
+				if (y == 10 && x == 1)
+					System.out.print(" " + space);
+				else if (x == 1)
+					System.out.print("  " + space);
+				else
+					System.out.print(" " + space);
 			}
 			System.out.print("\n");
 		}
 	}
-	
+
 }
