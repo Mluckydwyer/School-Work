8c4b668b7d8c3b37dcd2fddc93dfd93ffc82a514
Matt Dwyer <mluckydwyer@gmail.com>
2015-06-08 13:27:34 -0500

    Play check

--- School Programs/src/Battleship/BattleshipGame.java
+++ School Programs/src/Battleship/BattleshipGame.java
@@ -17,89 +17,170 @@ public class BattleshipGame {
 	private static Scanner keyboardInt = new Scanner(System.in);
 	private static Random random;
 
+	public static int cpuHitTotal;
+	public static int playerHitTotal;
+
 	public static void main(String[] args) {
-		game = new BattleshipGame();
-		random = new Random();
-		int choice;
-		int cordX = 0;
-		int cordY = 0;
+		boolean replay = false;
 
-		boolean redo = false;
-		boolean orientation = false; // true for horizontal, false for vertical
+		do {
+			game = new BattleshipGame();
+			random = new Random();
+			int choice;
+			int cordX = 0;
+			int cordY = 0;
+			cpuHitTotal = 0;
+			playerHitTotal = 0;
 
-		shipName[1] = "Patrol Boat";
-		shipName[2] = "Destroyer";
-		shipName[3] = "Submarine";
-		shipName[4] = "Battleship";
-		shipName[5] = "Aircraft Carrier";
+			boolean redo = false;
+			boolean orientation = false; // true for horizontal, false for
+											// vertical
+			redo = false;
 
-		shipLength[1] = 2;// TESTTTTTTTTTTTTTTTTT
-		shipLength[2] = 3;
-		shipLength[3] = 3;
-		shipLength[4] = 4;
-		shipLength[5] = 5;
+			shipName[1] = "Patrol Boat";
+			shipName[2] = "Destroyer";
+			shipName[3] = "Submarine";
+			shipName[4] = "Battleship";
+			shipName[5] = "Aircraft Carrier";
 
-		System.out.println("Hello and welcome to Batleship");
-		game.clearBoard();
-		game.cpuShipPlacement();
+			shipLength[1] = 2;// TESTTTTTTTTTTTTTTTTT
+			shipLength[2] = 3;
+			shipLength[3] = 3;
+			shipLength[4] = 4;
+			shipLength[5] = 5;
 
-		for (int x = 1; x < 6; x++) {
-			game.printBoard();
+			System.out.println("Hello and welcome to Batleship");
+			game.clearBoard();
+			game.cpuShipPlacement();
+
+			for (int x = 1; x < 6; x++) {
+				game.printBoard();
+
+				do {
+					redo = false;
+					System.out.println("How would you like to orient your " + shipName[x] + " that is " + shipLength[x] + " spaces long?");
+					System.out.println("[1] - Horizontaly (==)");
+					System.out.println("[2] - Verticaly (||)");
+					choice = keyboardInt.nextInt();
+
+					if (choice < 1 && choice > 2) {
+						redo = true;
+					} else {
+						if (choice == 1)
+							orientation = true; // true for horizontal, false
+												// for
+												// vertical
+						else if (choice == 2)
+							orientation = false; // true for horizontal, false
+													// for
+													// vertical
+					}
+
+					if (!redo) {
+						System.out.println("what cordinates would you like the uper left corrner of your " + shipName[x] + " that is " + shipLength[x] + " spaces long to be in (x then y)?");
+						cordX = keyboardInt.nextInt();
+						cordY = keyboardInt.nextInt();
+						// toggles the result, it does this because checkShip
+						// placement returns true if it is ok
+						redo = !game.checkShipPlacement(orientation, shipLength[x], cordX, cordY, "player");
+					}
+
+					if (redo) {
+						System.out.println("\f");
+						game.printBoard();
+						System.out.println("That isn't a valid loaction for that ship");
+					}
+
+				} while (redo);
+
+				for (int i = shipLength[x]; i > 0; i--) {
+
+					if (i == shipLength[x])
+						playerShips[cordX][cordY] = true;
+
+					if (orientation)
+						playerShips[cordX + (shipLength[x] - i)][cordY] = true;
+					else if (!orientation)
+						playerShips[cordX][cordY + (shipLength[x] - i)] = true;
+				}
+			}
+
+			game.cpuShipPlacement();
+
+			while (playerHitTotal < 17 && cpuHitTotal < 17) {
+				System.out.println("\f");
+
+				do {
+					redo = false;
+					game.printBoard();
+					System.out.println("What cordinates would you like to fire upon?");
+					cordX = keyboardInt.nextInt();
+					cordY = keyboardInt.nextInt();
+
+					if (cordX > 10 || cordX < 1 || cordY > 10 || cordY > 1)
+						redo = true;
+
+					if (redo) {
+						System.out.println("\f");
+						game.printBoard();
+						System.out.println("That isn't a valid loaction for that ship");
+					}
+				} while (redo);
+				playerHits[cordX][cordY] = true;
+
+				if (cpuShips[cordX][cordY]) {
+					System.out.println("The you hit the CPU's battleship at (" + cordX + "," + cordY + ")");
+					playerHitTotal++;
+				} else {
+					System.out.println("You missed at (" + cordX + "," + cordY + ")");
+				}
+
+				if (playerHitTotal < 17 && cpuHitTotal < 17)
+					break;
+				else
+					game.cpuShot();
+			}
+
+			if (playerHitTotal < 17)
+				System.out.println("\fYou Won!!!");
+			else if (cpuHitTotal < 17)
+				System.out.println("\fLost Won!!!");
 
 			do {
 				redo = false;
-				System.out.println("How would you like to orient your " + shipName[x] + " that is " + shipLength[x] + " spaces long?");
-				System.out.println("[1] - Horizontaly (==)");
-				System.out.println("[2] - Verticaly (||)");
+				System.out.println("would you like to play again (1:Yes, 2:No)?");
 				choice = keyboardInt.nextInt();
-
-				if (choice < 1 && choice > 2) {
-					redo = true;
-				} else {
-					if (choice == 1)
-						orientation = true; // true for horizontal, false for
-											// vertical
-					else if (choice == 2)
-						orientation = false; // true for horizontal, false for
-												// vertical
-				}
-
-				if (!redo) {
-					System.out.println("what cordinates would you like the uper left corrner of your " + shipName[x] + " that is " + shipLength[x] + " spaces long to be in (x then y)?");
-					cordX = keyboardInt.nextInt();
-					cordY = keyboardInt.nextInt();
-					// toggles the result, it does this because checkShip
-					// placement returns true if it is ok
-					redo = !game.checkShipPlacement(orientation, shipLength[x], cordX, cordY, "player");
-				}
-
-				if (redo) {
-					System.out.println("\f");
-					game.printBoard();
-					System.out.println("That isn't a valid loaction for that ship");
-				}
-
+				
+				if (choice == 1) replay = true;
+				else if (choice == 2) replay = false;	
+				else if (choice != 1 && choice != 2) redo = true;
+					
+					
 			} while (redo);
+			
+		} while (replay);
+		System.out.println("\f Thanks For Playing");
+	}
 
-			for (int i = shipLength[x]; i > 0; i--) {
+	private void cpuShot() {
+		int cordX = random.nextInt(10) + 1;
+		int cordY = random.nextInt(10) + 1;
 
-				if (i == shipLength[x])
-					playerShips[cordX][cordY] = true;
+		cpuHits[cordX][cordY] = true;
 
-				if (orientation)
-					playerShips[cordX + (shipLength[x] - i)][cordY] = true;
-				else if (!orientation)
-					playerShips[cordX][cordY + (shipLength[x] - i)] = true;
-			}
+		if (playerShips[cordX][cordY]) {
+			System.out.println("The CPU hit your battleship at (" + cordX + "," + cordY + ")");
+			cpuHitTotal++;
+		} else {
+			System.out.println("The CPU missed at (" + cordX + "," + cordY + ")");
 		}
-
-		game.cpuShipPlacement();
 	}
 
 	private void cpuShipPlacement() {
 		boolean redo = false;
 		int cordX;
 		int cordY;
+		// orin is true for horizontal, false for vertical
 		boolean orin;
 
 		for (int x = 1; x < 6; x++) {
@@ -134,23 +215,30 @@ public class BattleshipGame {
 			return false;
 
 		if (orin) {
-			if (cordX + length > 11) return false;
-			if (cordY > 11) return false;
+			if (cordX + length > 11)
+				return false;
+			if (cordY > 11)
+				return false;
 		} else if (!orin) {
-			if (cordX > 11) return false;
-			if (cordY + length > 11) return false;
+			if (cordX > 11)
+				return false;
+			if (cordY + length > 11)
+				return false;
 		}
 
 		if (player.equals("player")) {
 			for (int i = length; i > 0; i--) {
-				if (orin && playerShips[cordX + i - 1][cordY]) return false;
-				if (!orin && playerShips[cordX][cordY + i - 1]) return false;
+				if (orin && playerShips[cordX + i - 1][cordY])
+					return false;
+				if (!orin && playerShips[cordX][cordY + i - 1])
+					return false;
 			}
-		}
-		else if (player.equals("cpu")) {
+		} else if (player.equals("cpu")) {
 			for (int i = length; i > 0; i--) {
-				if (orin && cpuShips[cordX + i - 1][cordY]) return false;
-				if (!orin && cpuShips[cordX][cordY + i - 1]) return false;
+				if (orin && cpuShips[cordX + i - 1][cordY])
+					return false;
+				if (!orin && cpuShips[cordX][cordY + i - 1])
+					return false;
 			}
 		}
 		return true;
@@ -188,6 +276,8 @@ public class BattleshipGame {
 					space = "O";
 				} else if (!playerShips[x][y]) {
 					space = ".";
+				} else if (playerShips[x][y] && cpuHits[x][y]) {
+					space = "X";
 				}
 
 				if (y == 10 && x == 1)
@@ -205,6 +295,8 @@ public class BattleshipGame {
 					space = "O";
 				} else if (!cpuShips[x][y]) {
 					space = ".";
+				} else if (cpuShips[x][y] && playerHits[x][y]) {
+					space = "X";
 				}
 
 				if (y == 10 && x == 1)
